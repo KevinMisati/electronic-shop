27b9bd287a1fea44e0566199e244afe68bd67f37
kevinmisati <kevinmosigisi2001@gmail.com>
2021-11-07 11:11:05 +0300

    hdh

--- app/settings.py
+++ app/settings.py
@@ -155,9 +155,9 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = [
+""" STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'frontend/static'),
-    ]
+    ] """
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'

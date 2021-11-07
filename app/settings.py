85eec4ff6d27635affa584c4087704cbfc6a9997
kevinmisati <kevinmosigisi2001@gmail.com>
2021-11-07 13:48:56 +0300

    gd

--- app/settings.py
+++ app/settings.py
@@ -162,7 +162,7 @@ STATIC_URL = '/static/'
     ] """
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-#STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+#STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
     
 MEDIA_URL = 'media/'
 MEDIA_ROOT = os.path.join(BASE_DIR,'media')
@@ -173,10 +173,6 @@ MEDIA_ROOT = os.path.join(BASE_DIR,'media')
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 AUTH_USER_MODEL = "authentication.CustomUser"
-
-
-
-
 
 
 #django_heroku.settings(locals())

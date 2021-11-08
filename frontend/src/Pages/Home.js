419b5442459a898d95fa0922db153e3a39eedb3d
kevinmisati <kevinmosigisi2001@gmail.com>
2021-11-08 17:21:20 +0300

    j

--- frontend/src/Pages/Home.js
+++ frontend/src/Pages/Home.js
@@ -2,6 +2,7 @@ import React,{useEffect,useState} from 'react'
 import classes from "./Home.module.css"
 import Banner from '../components/Home/Banner'
 import TopSelling from '../components/Home/TopSelling'
+import axiosInstance from '../axiosApi'
 const Home = () => {
 
   const [products,setProducts] = useState([])

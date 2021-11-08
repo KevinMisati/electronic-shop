54e972cf86e40a502953c0adbd99afef98a21368
kevinmisati <kevinmosigisi2001@gmail.com>
2021-11-08 18:56:17 +0300

    dh

--- frontend/src/Pages/Home.js
+++ frontend/src/Pages/Home.js
@@ -5,24 +5,7 @@ import TopSelling from '../components/Home/TopSelling'
 import axiosInstance from '../axiosApi'
 const Home = () => {
 
-  const [products,setProducts] = useState([])
-  const [filteredProducts,setFilteredProducts] = useState([])
-alert("hello")
-  useEffect(() => {
-        axiosInstance.get("products/")
-        .then(response => {
-          alert("jnjk x djjfjf ejjj dkdkkd dkdkkdkdkdkdk")
-            setProducts(response.data)
-            setFilteredProducts(response.data.filter(product => product.category == "laptops"))
-            console.log(response.data)
-            alert("hello")
-            alert(response.data)
-        })
-        
-        .catch(error => {
-            throw error
-        })
-    },[])
+
 
     return (
         <div className={classes["home_container"]}>

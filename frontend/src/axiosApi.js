015295505701d33c6868761ac680cd1ad5ea5706
kevinmisati <kevinmosigisi2001@gmail.com>
2021-11-08 08:11:06 +0300

    gd

--- frontend/src/axiosApi.js
+++ frontend/src/axiosApi.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const baseUrl ="http://127.0.0.1:8000/api/"
+const baseUrl ="https://electron-shop.herokuapp.com/api/"
 const axiosInstance = axios.create({
     baseURL:baseUrl,
     timeout:5000,

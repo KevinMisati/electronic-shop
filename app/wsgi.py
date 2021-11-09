1ec71d6890be944b72b23a46a1131ceba789cb61
kevinmisati <kevinmosigisi2001@gmail.com>
2021-11-09 15:39:13 +0300

    dh

--- app/wsgi.py
+++ app/wsgi.py
@@ -7,10 +7,8 @@ For more information on this file, see
 https://docs.djangoproject.com/en/3.2/howto/deployment/wsgi/
 """
 
-import os
-
 from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'app.settings')
+from whitenoise.django import DjangoWhiteNoise
 
 application = get_wsgi_application()
+application = DjangoWhiteNoise(application)

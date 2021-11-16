d4e23f578b06f50bffdb96dedef25dc388b702d2
kevinmisati <kevinmosigisi2001@gmail.com>
2021-11-16 09:39:53 +0300

    jd

--- frontend/src/components/Account/Login.js
+++ frontend/src/components/Account/Login.js
@@ -1,4 +1,4 @@
-import React,{useState} from 'react'
+import React,{useState,useEffect} from 'react'
 import classes from "./Login.module.css"
 import {Link} from "react-router-dom"
 import axiosInstance from '../../axiosApi'
@@ -8,16 +8,27 @@ const Login = () => {
     let history = useHistory();
     const [email,setEmail] = useState("")
     const [password,setPassword] = useState("")
+    const [isformValid,setIsFormValid] = useState(false)
+    const [errMessage,setErrMessage] = useState("")
+
+
+    useEffect(() => {
+        console.log(email,password)
+        if (email !== ""){
+            setIsFormValid(true)
+        } 
+    })
     const handleSubmit = (e) => {
         e.preventDefault()
-        axiosInstance.post('token/obtain/',{
+        if (email !== "" && password !== "" && email.includes("@")){
+                axiosInstance.post('token/obtain/',{
                 email : email,
                 password : password
             }
         )
         .then(response => {
+            
             axiosInstance.defaults.headers['Authorization'] = 'JWT ' + response.data.acess
-            //localStorage.setItem('access_token',response.data.refresh)
             localStorage.setItem('refresh_token',response.data.refresh)
             localStorage.setItem('access_token',response.data.access)
 
@@ -31,6 +42,12 @@ const Login = () => {
         .catch (error => {
             throw error
         })
+            }
+
+        else{
+            setErrMessage("Either password or email is wrong.")
+        }
+        
     }
     const handleEmailChange = (e) => {
         setEmail(e.target.value)
@@ -38,18 +55,22 @@ const Login = () => {
     const handlePasswordChange = (e) => {
         setPassword(e.target.value)
     }
+   
     return (
         <div className={classes["login-container"]}>
             <div className={classes["login"]}>
                 <h1>Login</h1>
                 <form>
+                    {
+                        <p className={classes["err-message"]}>{errMessage}</p>
+                    }
                     <div className={classes["input-control"]}>
                         <label htmlFor='email'>email</label>
-                        <input onChange={handleEmailChange} id='email' type='email' name='email' value={email} />
+                        <input required={true} onChange={handleEmailChange} id='email' type='email' name='email' value={email} />
                     </div>
                     <div className={classes["input-control"]}>
                         <label htmlFor='password'>passowd</label>
-                        <input onChange={handlePasswordChange} id='password' type='password' name='password' value={password} />
+                        <input required={true} onChange={handlePasswordChange} id='password' type='password' name='password' value={password} />
                     </div>
                     <div className={classes["input-control"]}>
                         <button onClick={handleSubmit} className={classes["signin-btn"]}>sign in</button>
